dbf7e49409b98fd76d122480562e4df114bbcc11
jayjay <jinwoolee131@gmail.com>
2021-10-21 18:02:38 -0400

    added unsplash simple blog

---
+++ _frontend/unsplash-simple-blog.md
@@ -0,0 +1,116 @@
+---
+title: "Unsplash blog"
+excerpt: "vanilla javascript"
+layout: raw-page
+---
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Jay.Unsplash Blog</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Font awesome icon -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+  <link rel="stylesheet" href="/assets/unsplash-simple-blog/style.css">
+</head>
+<body>
+  <!-- header -->
+  <header>
+    <nav class = "navbar">
+      <div class = "container">
+        <a href = "https://jinlee487.github.io/frontend/simple-blog/" class = "navbar-brand">Jay.Unsplash</a>
+        <div class = "navbar-nav">
+          <a href = "https://jinlee487.github.io/">home</a>
+          <a href = "#about">about</a>
+        </div>
+      </div>
+    </nav>
+    <div class = "banner">
+      <div class = "container">
+        <h1 class = "banner-title">
+          <span>Jay.</span> Unsplash Image
+        </h1>
+        <p>every photos & images you want to search</p>
+        <form>
+          <input type = "text" class = "search-input" placeholder="find your images . . .">
+          <button type = "button" class = "search-btn" id = "search-btn">
+            <i class = "fas fa-search"></i>
+          </button>
+        </form>
+      </div>
+    </div>
+  </header>
+  <!-- end of header -->
+  <!-- design -->
+  <section class = "design" id = "design">
+    <div class = "container">
+      <div class = "title">
+        <h2>Random photos & images</h2>
+        <p>Random photos & images on Unsplash</p>
+      </div>
+      <div class = "design-content">
+        <!-- item -->
+        <!-- <div class = "design-item">
+          <div class = "design-img">
+            <img src = "" alt = "images/art-design-1.jpg">
+            <span><i class = "far fa-heart"></i> 22</span>
+            <span>Unsplash</span>
+          </div>
+          <div class = "design-title">
+            <a href = "#">make an awesome art portfolio for college or university</a>
+          </div>
+        </div> -->
+        <!-- end of item -->
+      </div>
+    </div>
+  </section>
+  <br>
+  <br>
+  <div class = "design-btns">
+    <button id = "design-item-add-btn"><i class = "fas fa-plus"></i></button>
+  </div>
+  <div class = "image-details">
+    <!-- image close btn -->
+    <button type = "button" class = "btn image-close-btn" id = "image-close-btn">
+      <i class = "fas fa-times"></i>
+    </button>
+    <!-- image content -->
+    <div class = "image-details-content">
+    </div>
+  </div>
+  <!-- end of design -->
+  <!-- about -->
+  <section class = "about" id = "about">
+    <div class = "container">
+      <div class = "about-content">
+        <div>
+          <img src = "assets/unsplash-simple-blog/images/about-me-bg.jpg" alt = "assets/unsplash-simple-blog/images/about-me-bg.jpg">
+        </div>
+        <div class = "about-text">
+          <div class = "title">
+            <h2>Jay Lee</h2>
+            <p>Software Development is my passion</p>
+          </div>
+          <p>I am a passionate software developer looking for new job opportunities! This is my personaly vanilla javascript project.</p>
+          <p>The purpose of this project is to practice vanilla javascript, CSS and HTML. Unsplash has many stock photos available for developers to use. The search API allows users to search for images much easier.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end of about -->
+  <!-- footer -->
+  <footer>
+    <div class = "social-links">
+      <!-- <a href = "#"><i class = "fab fa-facebook-f"></i></a>
+      <a href = "#"><i class = "fab fa-twitter"></i></a>
+      <a href = "#"><i class = "fab fa-instagram"></i></a>
+      <a href = "#"><i class = "fab fa-pinterest"></i></a> -->
+      <a href = "https://www.linkedin.com/in/jinwoolee487/"><i class = "fab fa-linkedin-in"></i></a>
+      <a href = "https://github.com/jinlee487"><i class = "fab fa-github"></i></a>
+    </div>
+    <span>Jay.Unsplash Blog Page</span>
+  </footer>
+  <!-- end of footer -->
+  <script src="assets/unsplash-simple-blog/script.js"></script> 
+</body>
+</html>
---
+++ assets/unsplash-simple-blog/script.js
@@ -0,0 +1,110 @@
+/* Fetch API */
+const designList = document.getElementById('design');
+const searchBtn = document.getElementById('search-btn');
+const imageDetailsContent = document.querySelector('.image-details-content');
+const imageCloseBtn = document.getElementById('image-close-btn');
+const client_id = "1OosJucB6P5xcZo-i-orjAIBg4heA-So7bzadHSmpGk";
+const imageAddBtn = document.getElementById('design-item-add-btn');
+
+// event listeners
+searchBtn.addEventListener('click',searchImg);
+designList.addEventListener('click', getImageDetail);
+imageCloseBtn.addEventListener('click', () => {
+    imageDetailsContent.parentElement.classList.remove('showImage');
+    scrollChange();
+});
+document.getElementsByTagName("form")[0].setAttribute("onsubmit","event.preventDefault();");
+document.getElementsByClassName("search-input")[0].addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        searchBtn.click();
+    }
+});
+
+window.addEventListener('DOMContentLoaded', loadImg);
+imageAddBtn.addEventListener('click',(e)=>{
+    loadImg(e,document.getElementsByClassName("search-input")[0].value);
+});
+
+function searchImg(e){
+    designList.getElementsByClassName("design-content")[0].innerHTML="";
+    const key=document.getElementsByClassName("search-input")[0].value;
+    loadImg(e,key);
+}
+function loadImg(e,query = 'random') {
+    const url = "https://api.unsplash.com/photos/random?client_id="+client_id+"&query="+query+"&count=10";
+    console.log(url);
+    fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            for (let i = 0; i < 9; i++) {
+                const design_item = `<div class = "design-item">
+                                        <div class = "design-img">
+                                            <img src = "${data[i+1].urls.regular}" imageId = "${data[i+1].id}" alt = "assets/unsplash-simple-blog/images/art-design-1.jpg">
+                                            <span><i class = "far fa-heart"></i>${data[i+1].likes}</span>
+                                            <span>Unsplash</span>
+                                        </div>
+                                    </div>`;
+                designList.getElementsByClassName("design-content")[0].innerHTML+=design_item;
+            }
+            document.getElementsByTagName("header")[0].style.background = "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url("+ data[0].urls.regular +") center/cover no-repeat fixed";
+    });
+}
+
+// get recipe of the meal
+function getImageDetail(e){
+    if(e.target.classList.contains('design-img')){
+        const imageId = e.target.getElementsByTagName("img")[0].getAttribute("imageid");
+        fetch(`https://api.unsplash.com/photos/${imageId}?client_id=${client_id}`)
+        .then(response => response.json())
+        .then(data => unsplashModal(data));
+    }
+}
+
+// create a modal
+function unsplashModal(designItem){
+    let loc = "";
+    if(designItem.location.city!==null&&designItem.location.country!==null){
+        loc += designItem.location.city + ", " + designItem.location.country;
+    } else if(designItem.location.city!==null){
+        loc += designItem.location.city;
+    } else if(designItem.location.country!==null){
+        loc += designItem.location.country;
+    }  
+    let desc = "";
+    if(designItem.description!==null){
+        desc += designItem.description;
+    } else {
+        desc += "--------";
+    }
+    const html = `
+        <div class = "image-img">
+            <img src = "${designItem.urls.full}" alt = "assets/unsplash-simple-blog/images/art-design-1.jpg">
+            <span><i class = "far fa-heart"></i>${designItem.likes}</span>
+            <span>Unsplash</span>
+        </div>
+        <h2 class = "image-title">${desc}</h2>
+        <div class = "image-instruct">
+            <h3>Posted: ${designItem.updated_at.split('T')[0]}</h3>
+            <p>${loc}</p>
+            <p class = "image-category">${designItem.user.username}</p>
+        </div>
+        <div class = "image-link">
+            <a href = "${designItem.links.download}" target = "_blank">Download Image</a>
+        </div>
+    `;
+    imageDetailsContent.innerHTML = html;
+    imageDetailsContent.parentElement.classList.add('showImage');
+    scrollChange();
+}
+function scrollChange(){
+    if (imageDetailsContent.parentElement.classList.contains("showImage")) {
+        // Disable scroll
+        document.querySelector("body").style.overflow = "hidden";
+    } else {
+        // Enable scroll
+        document.querySelector("body").style.overflow = "auto";
+    }
+}
